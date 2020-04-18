53fe4f5f99f911513d2a1f6045f25ee97b19cc7c
Evan Welsh <evan@Evans-MacBook-Pro-2.local>
2020-04-18 15:26:18 -0500

    Fix linting error, add core Firebase pods.

--- thermalcamera/LegalView.swift
+++ thermalcamera/LegalView.swift
@@ -28,7 +28,30 @@ class LegalView : UIView {
         field = UITextView()
         field.clipsToBounds = true
         field.translatesAutoresizingMaskIntoConstraints = false
-        field.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et risus lorem. Aenean at lacus non nunc hendrerit facilisis eget ut metus. In nisl est, mollis vitae arcu eget, vulputate tristique metus. Maecenas vel euismod augue. Suspendisse at orci vulputate, dictum augue vitae, vulputate sapien. Vestibulum eu lacinia sem. Pellentesque erat orci, tempor quis eleifend ut, suscipit sollicitudin neque. Sed iaculis sit amet velit vitae dictum. Etiam diam metus, tempus quis justo ac, consectetur mollis odio. Curabitur a purus blandit, consectetur lectus id, feugiat sem. Cras gravida lacus et magna rhoncus, at fringilla neque laoreet. Etiam tempus leo mi, sed bibendum ligula congue at. Cras aliquet, nisi ut tempor vehicula, dolor ex mollis nunc, non molestie lacus sem vel neque. Suspendisse imperdiet maximus elit, ac iaculis magna accumsan a. Vivamus arcu erat, rhoncus id commodo quis, auctor sit amet massa. Mauris mollis nunc quis semper malesuada. In euismod tortor eu massa vulputate, eget varius arcu semper. Curabitur venenatis justo lectus, a condimentum lorem aliquet a. Quisque non nunc elit. Mauris ultricies ipsum a massa fringilla, eget sagittis nibh mollis. Morbi eu urna convallis, ultricies ante vel, ultricies urna. Maecenas in felis nisl. Suspendisse odio orci, aliquet ac neque at, pellentesque pharetra purus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+        field.text = """
+                     Lorem ipsum dolor sit amet, consectetur
+                      adipiscing elit. Nullam et risus lorem. 
+                     Aenean at lacus non nunc hendrerit facilisis eget ut metus.
+                     In nisl est, mollis vitae arcu eget, vulputate tristique metus.
+                     Maecenas vel euismod augue. Suspendisse at orci vulputate, dictum
+                     augue vitae, vulputate sapien. Vestibulum eu lacinia sem.
+                     Pellentesque erat orci, tempor quis eleifend ut, suscipit
+                     sollicitudin neque. Sed iaculis sit amet velit vitae dictum.
+                     Etiam diam metus, tempus quis justo ac, consectetur mollis odio.
+                     Curabitur a purus blandit, consectetur lectus id, feugiat sem.
+                     Cras gravida lacus et magna rhoncus, at fringilla neque laoreet.
+                     Etiam tempus leo mi, sed bibendum ligula congue at.Cras aliquet,
+                     nisi ut tempor vehicula, dolor ex mollis nunc, non molestie lacus sem vel neque.
+                     Suspendisse imperdiet maximus elit, ac iaculis magna accumsan a.
+                     Vivamus arcu erat, rhoncus id commodo quis, auctor sit amet massa.
+                     Mauris mollis nunc quis semper malesuada.
+                     In euismod tortor eu massa vulputate, eget varius arcu semper.
+                     Curabitur venenatis justo lectus, a condimentum lorem aliquet a.
+                     Quisque non nunc elit. Mauris ultricies ipsum a massa fringilla, eget sagittis nibh mollis.
+                     Morbi eu urna convallis, ultricies ante vel, ultricies urna. Maecenas in felis nisl.
+                     Suspendisse odio orci, aliquet ac neque at, pellentesque pharetra purus.
+                     Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                     """
         
         addSubview(field)
         
